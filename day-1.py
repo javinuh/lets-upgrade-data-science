ae91091e4c3c7a6e014fa864327903a6559165e8
javinuh <63115223+javinuh@users.noreply.github.com>
2020-11-10 15:32:20 +0530

    day 1 assignment submitted

---
+++ day-1.py
@@ -0,0 +1,14 @@
+print("LETS UPGRADE")
+#Q-3 and 4
+cost_price=int(input())
+selling_price=int(input())
+if cost_price<selling_price:
+    print("Profit")
+elif cost_price>selling_price:
+    print("Loss")
+else:
+    print("Neither")
+#Q-5
+euro = int(input())
+rupees = euro*80
+print(rupees)
